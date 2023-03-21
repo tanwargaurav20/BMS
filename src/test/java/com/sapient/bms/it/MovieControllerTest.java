8172f16b85c2f2741586ce462a84fdaec731fe3d
Gaurav Tanwar <tanwar.gaurav.20@gmail.com>
2023-03-21 00:02:02 +0000

    chnages to pick test properties as separate

--- src/test/java/com/sapient/bms/it/MovieControllerTest.java
+++ src/test/java/com/sapient/bms/it/MovieControllerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class MovieControllerTest {
 
     private final String createMovieRequestPayload =
